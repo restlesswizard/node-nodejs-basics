9b9d557501d6ec530f156976538587e01575b14e
restlesswizard <erxme@yandex.ru>
2024-01-29 13:23:05 +0500

    feat: finish subtask 'compress.js'

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import * as zlib from 'zlib'
+import * as fs from 'fs'
+
+const zip = zlib.createGzip();
+
 const compress = async () => {
-    // Write your code here 
+    let read = fs.createReadStream('./src/zip/files/fileToCompress.txt');
+    let write = fs.createWriteStream('./src/zip/files/archive.gz') 
+    read.pipe(zip).pipe(write)
 };
 
 await compress();
