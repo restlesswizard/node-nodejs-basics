2bb5d8a187735c775ec98fa6ecb27b4ac22ea6eb
restlesswizard <erxme@yandex.ru>
2024-01-29 14:03:22 +0500

    feat: finish subtask 'decompress.js'

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import * as zlib from 'zlib'
+import * as fs from 'fs'
+
+const unzip = zlib.createUnzip()
+
 const decompress = async () => {
-    // Write your code here 
+    let read = fs.createReadStream('./src/zip/files/archive.gz')
+    let write = fs.createWriteStream('./src/zip/files/fileToCompress.txt')
+    read.pipe(unzip).pipe(write)
+
 };
 
 await decompress();
