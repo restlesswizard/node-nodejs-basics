65ca76d9245d179d4457ece7fc87e99910e5a529
restlesswizard <erxme@yandex.ru>
2024-01-29 14:25:51 +0500

    feat: start subtask 'write.js'

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs'
+import {stdin, stdout} from 'node:process'
+
 const write = async () => {
-    // Write your code here 
+    const file = './src/streams/files/fileToWrite.txt';
+
+    // const readFile = fs.createReadStream(file)
+    // const writeFile = fs.createWriteStream(file);
+    // writeFile.write('yolo!')
+
 };
 
 await write();
